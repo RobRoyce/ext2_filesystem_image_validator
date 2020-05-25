909a338bcc8c5e3185c64bb3d3d91d481fe7a6bd
Tyler Hackett <ins10102@gmail.com>
2020-05-24 22:40:06 -0700

    Add ImageReader class and naive example implementation

---
+++ bufferedimagereader.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <fstream>
+
+#include "imagereader.hpp"
+
+// -------------------------------------------------- EXT2 Image Reader Class
+//
+// Reads an EXT2 Image from standard File I/O into buffers.
+//
+// Better suited to large images that may not fit into memory.
+//
+class BufferedImageReader : public ImageReader {
+ public:
+  BufferedImageReader(char*);
+  ~BufferedImageReader();
+
+  virtual int init();
+
+  struct ext2_super_block *getSuperBlock();
+
+  virtual void *getBlock(size_t);
+
+  virtual void *getBlockGroup(size_t);
+
+protected:
+
+  virtual int readSuperBlock();
+
+private:
+  
+  std::ifstream *fs;
+
+  char *blockBuffer;
+
+  char *blockGroupBuffer;
+
+  size_t blockSize;
+  size_t blockGroupSize;
+
+};
