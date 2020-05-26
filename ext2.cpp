626b2b9ef90bcf2e7d8e4b130fad671eb0d711ff
Rob Royce <robertris2@gmail.com>
2020-05-25 17:48:14 -0700

    Reworked meta file structs. Correct error detection on init

--- ext2.cpp
+++ ext2.cpp
@@ -1,95 +1,242 @@
 #include "ext2.hpp"
 
-
 EXT2::EXT2(char *filename) {
-  this->metaFileName = std::string(filename);
-  int rc = stat(filename, &this->metaFileStat);
-  if (rc != 0)
-    throw "Invalid File";
+  // -------------------------------------------------- Initial Meta Check
+  try {
+    meta = std::make_unique<MetaFile>();
+  } catch (...) {
+    throw runtime_error("FileSystemAllocationError");
+  }
 
-  this->superBlock = new ext2_super_block;
+  meta->filename = std::string(filename);
+
+  if (stat(meta->filename.c_str(), &meta->stat) != 0)
+    throw runtime_error("FileSystemReadError");
+
+  // -------------------------------------------------- Init Super Block
+  try {
+    superBlock = std::make_unique<ext2_super_block>();
+  } catch (...) {
+    throw runtime_error("SuperBlockAllocationError");
+  }
+
+  if (!readSuperBlock())
+    throw runtime_error("SuperBlockReadError");
+
+  if (!parseSuperBlock())
+    throw runtime_error("SuperBlockParseError");
+
+  // -------------------------------------------------- Integrity Check Meta
+  if(!getMetaFileInfo())
+    throw runtime_error("FileSystemInvalidError");
+
+  if(!validateSuperBlock())
+    throw runtime_error("SuperBlockInvalidError");
 }
 
-EXT2::~EXT2() {
-  delete this->superBlock;
+// --------------------------------------------------Destructor
+EXT2::~EXT2() {}
+
+
+bool EXT2::getMetaFileInfo() {
+  // TODO implementation
+  return true;
 }
 
-int EXT2::readSuperBlock() {
+
+bool EXT2::readSuperBlock() {
   std::stringstream buffer;
-  std::ifstream fs(this->metaFileName, std::ios::binary | std::ios::in);
+  std::ifstream fs(meta->filename, std::ios::binary | std::ios::in);
   if (!fs)
-    return -1;
+    return false;
 
-  char buf[sizeof(ext2_super_block)];
+  const __u32 SBSIZE = sizeof(ext2_super_block);
+  if(SBSIZE != KiB)
+    return false;
 
-  fs.unsetf(std::ios::skipws);
-  fs.seekg(KiB, std::ios::beg);
-  fs.read(buf, sizeof(ext2_super_block));
-  memcpy(this->superBlock, buf, sizeof(ext2_super_block));
+  char buf[SBSIZE];
+  try {
+    fs.unsetf(std::ios::skipws);
+    fs.seekg(KiB, std::ios::beg);
+    fs.read(buf, SBSIZE);
+    if (!fs)
+      return false;
+  } catch (...) {
+    return false;
+  }
 
-  return 0;
+  if(memcpy(superBlock.get(), buf, SBSIZE) == nullptr)
+    return false;
+
+  return true;
 }
 
-int EXT2::parseSuperBlock() {
-  // -- There are various things we need to do based on contents of the Super Block
+
+bool EXT2::parseSuperBlock() {
+  // Attempt to perform any type of inferential or explicit validation using the
+  // specifications given for EXT2. We're mostly looking for inconsistencies here.
+
+
+  // Block size can be calculated in two ways. First, it should be the total
+  // file size divided by the number of blocks (file.size /
+  // superBlock.blockCount) Second, it can be found explicitly using
+  // (blockSize = 1024 << log_block_size) We can use this for validatiion
+  // purposes.
+  __u32 blockSize1 = meta->stat.st_size / superBlock->s_blocks_count;
+  __u32 blockSize2 = KiB << superBlock->s_log_block_size;
+
+  if(debug)
+    printf("Blocksize Calculations: [1:%d] [2:%d]...\n", blockSize1, blockSize2);
+  if (blockSize1 != blockSize2)
+    return false;
+
+
+  meta->blockCount = superBlock->s_blocks_count; // how many blocks in the file system?
+  meta->blockSize = blockSize2; // what size is each block?
+  meta->inodesPerGroup = superBlock->s_inodes_per_group; // How many inodes per group?
+  meta->blocksPerGroup = superBlock->s_blocks_per_group; // how many blocks per group?
+  meta->blockGroupSize = meta->blockSize * meta->blocksPerGroup; // what size is each block group?
+
+  if (debug) {
+    printf("Block Count: %d...\n", meta->blockCount);
+    printf("Block Size: %d...\n", meta->blockSize);
+    printf("Inodes Per Group: %d...\n", meta->inodesPerGroup);
+    printf("Blocks Per Group: %d...\n", meta->blocksPerGroup);
+    printf("Block Group Size: %lld...\n", meta->blockGroupSize);
+  }
+
+  // -- Note: the spec for project3a explicitly states that there will only be a single
+  // -- block group, so we can make some assumptions about valid input
+  // how many block groups are there?
+  meta->blockGroupsCount = (meta->blockCount < meta->blocksPerGroup) ? 1 : meta->blockCount / meta->blocksPerGroup;
+
+  if(debug)
+    printf("Number of Block Groups: %d...\n", meta->blockGroupsCount);
+  if (meta->blockGroupsCount != 1)
+    return false;
+
+
+  // -- There are various things we need to do based on contents of the Super
+  // Block
   // -- Revision determines algorithm for lookup (hash map vs linked list)
   // -- Revision 1:
   // --- variable inode sizes
   // --- extended attributesa
   // --- Sparse SuperBlocks
+  meta->rev = superBlock->s_rev_level;
 
-  this->rev = this->superBlock->s_rev_level;
-  this->block_size = this->metaFileStat.st_size / this->superBlock->s_blocks_count;
+  if(debug)
+    printf("Revision Level: %d...\n", meta->rev);
 
-  // TODO this field only exists in rev1 ext2, need to figure out how to compute
-  // it manually instead
-  this->inode_size = this->superBlock->s_inode_size;
-  return 0;
+  switch(meta->rev) {
+    case EXT2_OLD_REV:
+      
+      break;
+    case EXT2_DYNAMIC_REV:
+      meta->inodeSize = superBlock->s_inode_size;
+
+      break;
+    default:
+    break;
+  }
+
+  return true;
 }
 
 
 void EXT2::printSuperBlock() {
   printf("SUPERBLOCK,%d,%d,%d,%d,%d,%d,%d\n",
-         this->superBlock->s_blocks_count,
-         this->superBlock->s_inodes_count,
-         this->block_size,
-         this->inode_size,
-         this->superBlock->s_blocks_per_group,
-         this->superBlock->s_inodes_per_group,
-         this->superBlock->s_first_ino
+         superBlock->s_blocks_count,
+         superBlock->s_inodes_count,
+         meta->blockSize,
+         meta->inodeSize,
+         superBlock->s_blocks_per_group,
+         superBlock->s_inodes_per_group,
+         superBlock->s_first_ino
          );
 
+
   // TODO: Remove everything below
-  // cout << "inodes count: " << this->superBlock->s_inodes_count << endl;
-  // cout << "blocks count: " << this->superBlock->s_blocks_count << endl;
-  // cout << "reserved blocks count: " << this->superBlock->s_r_blocks_count << endl;
-  // cout << "free blocks count: " << this->superBlock->s_free_blocks_count << endl;
-  // cout << "free inodes count: " << this->superBlock->s_free_inodes_count << endl;
-  // cout << "first data block: " << this->superBlock->s_first_data_block << endl;
-  // cout << "log block size: " << this->superBlock->s_log_block_size << endl;
-  // cout << "log frag size: " << this->superBlock->s_log_frag_size << endl;
-  // cout << "blocks per group: " << this->superBlock->s_blocks_per_group << endl;
-  // cout << "frags per group: " << this->superBlock->s_frags_per_group << endl;
-  // cout << "inodes per group: " << this->superBlock->s_inodes_per_group << endl;
-  // cout << "mount time: " << this->superBlock->s_mtime << endl;
-  // cout << "write time: " << this->superBlock->s_wtime << endl;
-  // cout << "mount count: " << this->superBlock->s_mnt_count << endl;
-  // cout << "max mount count: " << this->superBlock->s_max_mnt_count << endl;
-  // cout << "magic signature: " << this->superBlock->s_magic << endl;
-  // cout << "file system state: " << this->superBlock->s_state << endl;
-  // cout << "errors: " << this->superBlock->s_errors << endl;
-  // cout << "minor revision level: " << this->superBlock->s_minor_rev_level << endl;
-  // cout << "time of last check: " << this->superBlock->s_lastcheck << endl;
-  // cout << "max time between checks: " << this->superBlock->s_checkinterval << endl;
-  // cout << "creator OS: " << this->superBlock->s_creator_os << endl;
-  // cout << "revision level: " << this->superBlock->s_rev_level << endl;
-  // cout << "default uid for reserved blocks: " << this->superBlock->s_def_resuid << endl;
-  // cout << "default gid for reserved blocks: " << this->superBlock->s_def_resgid << endl;
-  // cout << "first non-reserved inode: " << this->superBlock->s_first_ino << endl;
-  // cout << "size of inode structure: " << this->superBlock->s_inode_size << endl;
-  // cout << "compatible feature set: " << this->superBlock->s_feature_compat << endl;
-  // cout << "incompatible feature set: " << this->superBlock->s_feature_incompat << endl;
-  // cout << "readonly-compatible feature set: " << this->superBlock->s_feature_ro_compat << endl;
-  // cout << "Reserved padding (size: " << sizeof(this->superBlock->s_reserved)
-  //      << "): " << this->superBlock->s_reserved << endl;
+  cout << "inodes count: " << superBlock->s_inodes_count << endl;
+  cout << "blocks count: " << superBlock->s_blocks_count << endl;
+  cout << "reserved blocks count: " << superBlock->s_r_blocks_count << endl;
+  cout << "free blocks count: " << superBlock->s_free_blocks_count << endl;
+  cout << "free inodes count: " << superBlock->s_free_inodes_count << endl;
+  cout << "first data block: " << superBlock->s_first_data_block << endl;
+  cout << "log block size: " << superBlock->s_log_block_size << endl;
+  cout << "log frag size: " << superBlock->s_log_frag_size << endl;
+  cout << "blocks per group: " << superBlock->s_blocks_per_group << endl;
+  cout << "frags per group: " << superBlock->s_frags_per_group << endl;
+  cout << "inodes per group: " << superBlock->s_inodes_per_group << endl;
+  cout << "mount time: " << superBlock->s_mtime << endl;
+  cout << "write time: " << superBlock->s_wtime << endl;
+  cout << "mount count: " << superBlock->s_mnt_count << endl;
+  cout << "max mount count: " << superBlock->s_max_mnt_count << endl;
+  cout << "magic signature: " << superBlock->s_magic << endl;
+  cout << "file system state: " << superBlock->s_state << endl;
+  cout << "errors: " << superBlock->s_errors << endl;
+  cout << "minor revision level: " << superBlock->s_minor_rev_level << endl;
+  cout << "time of last check: " << superBlock->s_lastcheck << endl;
+  cout << "max time between checks: " << superBlock->s_checkinterval << endl;
+  cout << "creator OS: " << superBlock->s_creator_os << endl;
+  cout << "revision level: " << superBlock->s_rev_level << endl;
+  cout << "default uid for reserved blocks: " << superBlock->s_def_resuid << endl;
+  cout << "default gid for reserved blocks: " << superBlock->s_def_resgid << endl;
+  cout << "first non-reserved inode: " << superBlock->s_first_ino << endl;
+  cout << "size of inode structure: " << superBlock->s_inode_size << endl;
+  cout << "compatible feature set: " << superBlock->s_feature_compat << endl;
+  cout << "incompatible feature set: " << superBlock->s_feature_incompat << endl;
+  cout << "readonly-compatible feature set: " << superBlock->s_feature_ro_compat << endl;
+  cout << "Reserved padding (size: " << sizeof(superBlock->s_reserved)
+       << "): " << superBlock->s_reserved << endl;
+}
+
+
+void EXT2::printGroupSummary() {
+
+}
+
+void EXT2::printFreeBlockEntries(){}
+void EXT2::printFreeInodeEntries(){}
+void EXT2::printInodeSummary(){}
+void EXT2::printDirectoryEntries(){}
+void EXT2::printIndirectBlockRefs(){}
+
+
+
+bool EXT2::validateSuperBlock() {
+  // Magic Number Check
+  // EXT2 uses the magic number 0xEF53
+  if(superBlock->s_magic != EXT2_SUPER_MAGIC)
+    return false;
+
+  // The major revision number will inform the DS&A we use to read the FS. Here,
+  // we check the revision number and set specific flags that we will use during
+  // reading and parsing.
+  __u32 rev = superBlock->s_rev_level;
+
+  switch(rev) {
+    case 0: // Revision 0
+      if(superBlock->s_first_ino != EXT2_GOOD_OLD_FIRST_INO)
+        return false;
+
+      break;
+
+    case 1: // Revision 1
+      break;
+
+    default: // Error
+      return false;
+  }
+
+  // Block size can be calculated in two ways. First, it should be the total
+  // file size divided by the number of blocks (file.size /
+  // superBlock.blockCount) Second, it can be found explicitly using (blockSize
+  // = 1024 << log_block_size) We can use this for validatiion purposes.
+  __u32 blockSize1 = meta->stat.st_size / superBlock->s_blocks_count;
+  __u32 blockSize2 = KiB << superBlock->s_log_block_size;
+  if (blockSize1 != blockSize2)
+    return false;
+
+  return true;
 }
--- ext2.hpp
+++ ext2.hpp
@@ -15,6 +15,8 @@
 
 #define KiB 1024
 #define SUPERBLOCK_SIZE sizeof(ext2_super_block)
+#define EXT2_OLD_REV 0
+#define EXT2_DYNAMIC_REV 1
 
 extern int debug;
 
@@ -22,30 +24,131 @@ using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
+using std::runtime_error;
+
+// -------------------------------------------------- Meta File Info
+struct MetaFileLimits {
+  // Limits enforced by block size
+  // Based on Table 2.1 at
+  // https://www.nongnu.org/ext2-doc/ext2.html#behaviour-flags
+  unsigned int fileSystemBlocks;
+  unsigned int blocksPerBlockGroup;
+  unsigned int inodesPerBlockGroup;
+  unsigned int bytesPerBlockGroup;
+  unsigned long fileSystemSizeReal;
+  unsigned long fileSystemSizeLinux;
+  unsigned long blocksPerFile;
+  unsigned long long fileSizeReal;
+  unsigned long long fileSizeLinux;
+};
 
 
-// -------------------------------------------------- EXT2 Class
-class EXT2 {
+
+/**
+   MetaFile contains pertinent data fields regarding the actual EXT2 file
+   system. There are 2 levels of meta here, each with their own fields:
+
+   1 -- The actual file we are reading
+     -- stat, filename
+   2 -- The interpretation of that file
+     -- else (not stat, filename)
+
+     Level 1 can be established without reading the superBlock, whereas level 2
+     fields require fields from the superBlock. Level 2 fields are essentially
+     for convenience, since they can easily be retrieved from the superBlock or
+     the meta stat. Note that level 2 fields come before level 1 fields, due to
+     the fact that level 1 fields (especially the filename) are variable in
+     length, and so belong at the end of the struct to ensure optimal memory
+     footprint.
+ */
+struct MetaFile {
  public:
-   // Member Variables
+  // Level 2
+  __u32 blockCount;
+  __u32 blockSize;
+
+  __u32 inodeSize;
+  __u32 inodesPerGroup;
+
+  __u32 blocksPerGroup;
+  __u32 blockGroupsCount;
+  __u32 blockGroupsPerDescTable;
+
+  __u32 rev;
+
+  unsigned long long blockGroupSize;
+
+  struct stat stat;
+  std::string filename;
+
+  MetaFile() {}
+  ~MetaFile() {}
+
+private:
+  MetaFileLimits limits;
+
+};
 
 
-   // Methods
-   EXT2(char *);
-  ~EXT2();
-   int readSuperBlock();
-   int parseSuperBlock();
-   void printSuperBlock();
+
+// -------------------------------------------------- Prototypical Block
+class MemoryBlock {
+ public:
+   MemoryBlock() {}
+   MemoryBlock(std::vector<char> data) : data(data), size(data.size()) {}
 
  private:
-   // Member Variables
-  std::shared_ptr<ext2_super_block> sblock;
-  ext2_super_block *superBlock;
-   // struct ext2_super_block *superBlock;
-   struct stat metaFileStat;
-   std::string metaFileName;
-   __u32 rev;
-  __u32 block_size;
-  __u32 inode_size;
-   // Methods
+  std::vector<char> data;
+  __u32 size;
+};
+
+
+class MemoryBlockCache {
+ public:
+   MemoryBlockCache() {}
+   MemoryBlockCache(size_t nblocks) {
+     blocks.reserve(nblocks);
+   }
+   ~MemoryBlockCache() {}
+
+ private:
+  std::vector<MemoryBlock> blocks;
+
+};
+
+enum MemoryManagement {
+  DMA, MMIO, HYBRID
+};
+
+
+// -------------------------------------------------- EXT2
+class EXT2 {
+ public:
+  // Member Variables
+
+
+  // Methods
+  EXT2(char *);
+  ~EXT2();
+  bool readSuperBlock(); // validate and populate superBlock
+  bool parseSuperBlock(); // validate and populate metaFile
+
+  void printSuperBlock();
+  void printGroupSummary();
+  void printFreeBlockEntries();
+  void printFreeInodeEntries();
+  void printInodeSummary();
+  void printDirectoryEntries();
+  void printIndirectBlockRefs();
+
+
+ private:
+  // Member Variables
+  std::unique_ptr<ext2_super_block> superBlock;
+  std::unique_ptr<MetaFile> meta;
+  std::unique_ptr<MemoryBlockCache> blocks;
+
+  // Methods
+  bool getMetaFileInfo();
+  bool validateSuperBlock();
 };
